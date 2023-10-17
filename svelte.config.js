b5fa7f1ac2f23548f063e37d71349c354b28bdab
Xinnan-Alex <leongxinnan@outlook.my>
2023-10-17 22:28:10 +0800

    configurating gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: adapter(),
+
+    paths: {
+      base:
+        process.env.NODE_ENV === "production"
+          ? "/personal-portfolio-svelte"
+          : "",
+    },
   },
-  preprocess: vitePreprocess(),
 };
+
 export default config;
