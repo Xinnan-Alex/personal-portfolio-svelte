f2664cd10b4ba7cb05a4c8bf4e4108ba152fb1f3
Xinnan-Alex <leongxinnan@outlook.my>
2023-10-17 18:20:05 +0800

    portfolio template code

---
+++ src/componenets/Footer.svelte
@@ -0,0 +1,26 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b>leongxinnan@outlook.my</p>
+        <p>
+            <b class="pr-2">Linkedin</b>
+            <a href="https://www.linkedin.com/in/leongxinnan/" class="text-violet-400" target="_blank">Leong Xin Nan
+                <sup><span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]">
+                    </i>
+                </span></sup>
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">Github</b>
+            <a href="https://github.com/Xinnan-Alex/" class="text-violet-400" target="_blank">Xinnan-Alex
+                <sup><span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]">
+                    </i>
+                </span></sup>
+            </a>
+        </p>
+    </div>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Main from '../componenets/Main.svelte'
+</script>
+
+<Main />
